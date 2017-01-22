072853cafe00342c001d8093a5b85cda317de6d4
Solame <solameleong@outlook.com>
2017-01-22 14:48:08 +0800

    Fixed the fifth round button.

--- LilligantCup2016/main.js
+++ LilligantCup2016/main.js
@@ -8,7 +8,7 @@ window.onload = function () {
                 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V']
         },
         methods: {
-            generate_id: function(number) {
+            generate_id: function (number) {
                 var text = '#group'.concat(number);
                 return text;
             }
@@ -52,50 +52,59 @@ window.onload = function () {
                 }
                 return;
             },
-            get_group: function(group) {
+            get_group: function (group) {
                 return 'group'.concat(group);
             }
         }
     })
     var c = v = new Vue({
-        el: '#swiss_pas',
-        data: {
-            round: null,
-            plays: [],
-            current: 3,
-            swiss_matches: [],
-            swiss_players: []
-        },
-        mounted: function () {
-            this.$http.get('data/information.json').then(function (response) {
-                return response.json()
-            }).then(function (data) {
-                this.round = 5;
-                this.plays = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-                this.swiss_players = data.swiss_players;
-                this.swiss_matches = data.swiss_match;
-            })
-        },
-        methods: {
-            get_round: function(r) {
-                var round = ["一", "二", "三", "四", "五"];
-                var tmp = "第".concat(round[r - 1]);
-                return tmp.concat("轮");
+            el: '#swiss_pas',
+            data: {
+                round: null,
+                plays: [],
+                current: 3,
+                swiss_matches: [],
+                swiss_players: []
             },
-            get_name: function (index, round) {
-                var tmp = this.swiss_matches[round][index];
-                if (tmp === null) return"";
-                console.log(tmp);
-                tmp = Number(tmp);
-                return this.swiss_players[tmp].id;
+            mounted: function () {
+                this.$http.get('data/information.json').then(function (response) {
+                    return response.json()
+                }).then(function (data) {
+                    this.round = 5;
+                    this.plays = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+                    this.swiss_players = data.swiss_players;
+                    this.swiss_matches = data.swiss_match;
+                })
             },
-            get_marks: function (index,round) {
-                var tmp = this.swiss_matches[round][index];
-                if (tmp === null) return"";
-                console.log(tmp);
-                tmp = Number(tmp);
-                return this.swiss_players[tmp].marks;
+            methods: {
+                get_round: function (r) {
+                    var round = ["一", "二", "三", "四", "五"];
+                    var tmp = "第".concat(round[r - 1]);
+                    return tmp.concat("轮");
+                },
+                get_name: function (index, round) {
+                    var present = 3;
+                    if (round > present) return '';
+                    var tmp = this.swiss_matches[round][index];
+                    if (tmp === null) return "";
+                    tmp = Number(tmp);
+                    return this.swiss_players[tmp].id;
+                },
+                get_marks: function (index, round) {
+                    var present = 3;
+                    if (round > present) return "";
+                    var tmp = this.swiss_matches[round][index];
+                    if (tmp === null) return "";
+                    tmp = Number(tmp);
+                    var arr = this.swiss_players[tmp].marks;
+                    var text = '';
+                    for (var i = 0; i < 3; i++) {
+                        text = text.concat(arr[i].toString().concat('/'));
+                    }
+                    return text.slice(0, -1);
+                }
             }
         }
-    })
-};
+    )
+}
+;
